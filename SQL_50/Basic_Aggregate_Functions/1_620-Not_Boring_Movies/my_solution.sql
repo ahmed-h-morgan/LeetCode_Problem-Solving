9398413313befe8c18d26e54535267c18669a5d5
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2025-08-10 13:57:41 +0300

    50_SQL -  Basic Aggregate Functions -  task 01

---
+++ SQL_50/Basic_Aggregate_Functions/1_620-Not_Boring_Movies/my_solution.sql
@@ -0,0 +1,8 @@
+/*Write your MySQL query statement below:
+ - Query runtime: 242ms 
+ - Beats: 49.60%
+*/
+select id, movie, description, rating
+from cinema
+where id % 2  != 0 and description != 'boring'
+order by rating DESC
